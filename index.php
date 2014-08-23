355ce5d07b33d3a99db6d8ed1216e34dfabccc9c
Brendan Crawford <bcgir87@gmail.com>
2014-08-23 09:50:43 -0700

    changed the date formatting for blog posts

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <?php while(have_posts()): the_post()?>
 
         <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
-        <?php the_date('Y-m-d', '<h3>', '</h3>');?>
+        <?php the_date('F j, Y', '<h3>', '</h3>');?>
         <?php the_content(__('Continue Reading'));?>
 
     <?php endwhile;?>
