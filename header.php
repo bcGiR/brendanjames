c4bb09de8e2df4278585dc0fc52241c7e39562f1
Brendan Crawford <bcgir87@gmail.com>
2014-08-26 12:43:30 -0700

    added meta tags, removed some dead css

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Personal website of Brendan James Crawford, Student and Software Developer">
     <title><?php bloginfo('title')?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
